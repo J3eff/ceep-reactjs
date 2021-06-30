85cea2c555b008c241fae55ad5d6d791e3bc93b7
J3eff <jeffersonvideo125@gmail.com>
2021-06-30 20:12:46 -0300

    Inicio curso alura

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import CardNotas from './CardNotas';
+
+class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    <CardNotas />
+                </li>
+                <li>
+                    <CardNotas />
+                </li>
+                <li>
+                    <CardNotas />
+                </li>
+            </ul>
+        );
+    }
+}
+
+export default ListaDeNotas;
