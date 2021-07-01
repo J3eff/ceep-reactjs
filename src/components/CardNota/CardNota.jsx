d446b779f658dd3c12a0c2e9b87b452eff1894ac
J3eff <jeffersonvideo125@gmail.com>
2021-07-01 08:42:06 -0300

    conflitos de nomes

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import './CardNota.css';
+import './estilo.css';
 
 class CardNota extends Component {
 
     render() {
         return (
             <section className="card-nota">
-                <header>
-                    <h3>Titulo</h3>
+                <header className="card-nota_cabecalho">
+                    <h3 className="card-nota_titulo">Titulo</h3>
                 </header>
-                <p>Escreva sua nota</p>
+                <p className="card-nota_texto">Escreva sua nota</p>
             </section>
         );
     }
