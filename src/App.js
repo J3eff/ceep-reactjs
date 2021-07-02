d1adb32245ac3c60f98eea4320755ac3ccb9c605
J3eff <jeffersonvideo125@gmail.com>
2021-07-02 10:13:44 -0300

    organizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
+import './assets/App.css'
 
-function App() {
-  return (
-    
-  );
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
