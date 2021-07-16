fe12378284692aa007318cc4aa8391088bdbe7e0
J3eff <jeffersonvideo125@gmail.com>
2021-07-16 00:31:56 -0300

    Ciclo de vidas dos objetos

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -3,6 +3,14 @@ import './estilo.css';
 
 
 class ListaDeCategorias extends Component {
+    
+    componentDidMount(){
+        this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    }
+
+    _novasCategorias(categorias){
+        console.log(categorias)
+    }
 
     _handlerEventoIput(ev) {
         if (ev.key === "Enter") {
@@ -16,7 +24,7 @@ class ListaDeCategorias extends Component {
         return (
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
-                    {this.props.categorias.map((categoria, index) => {
+                    {this.props.categorias.categorias.map((categoria, index) => {
                         return <li key={index} className="lista-categorias_item" >{categoria}</li>
                     })}
 
